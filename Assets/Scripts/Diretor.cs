b50738c44dcd1f3dcfbe4db06e283703f5593e5a
Alex Ferreira Santos <alexsantos@pecege.com>
2024-06-09 12:29:34 -0300

    feat: adicionando tela de game over ao finalizar o jogo

--- Assets/Scripts/Diretor.cs
+++ Assets/Scripts/Diretor.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Diretor : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject imagemGameOver;
     public void FinalizarJogo(){
         Time.timeScale = 0;
+        imagemGameOver.SetActive(true);
     }
 }
