92779d3caa4aca3235091aadf1084ac5e29f7ba3
Alex Ferreira Santos <alexsantos@pecege.com>
2024-06-09 16:39:58 -0300

    feat: adicionando som ao pontua e trilha sonora

--- Assets/Scripts/Pontuacao.cs
+++ Assets/Scripts/Pontuacao.cs
@@ -8,13 +8,21 @@ public class Pontuacao : MonoBehaviour
     [SerializeField]
     private Text textoPontuacao;
     private int pontos;
+    private AudioSource audioPontuacao;
+
+    private void Awake()
+    {
+        audioPontuacao = GetComponent<AudioSource>();
+    }
     public void AdicionarPontos()
     {
         pontos++;
         textoPontuacao.text = pontos.ToString();
+        audioPontuacao.Play();
     }
 
-    public void Reiniciar(){
+    public void Reiniciar()
+    {
         pontos = 0;
         textoPontuacao.text = pontos.ToString();
     }
